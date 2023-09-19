99368947abc234e15806672f067813d0bda683f9
Akhil Mishra <akhilmishra184@gmail.com>
2023-09-20 02:30:18 +0530

    added date format function along with updated css

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,21 +1,53 @@
 import React from "react";
 
 const Card = ({ data, loading }) => {
+  const formattedDate = (time) => {
+    const timestamp = time * 1000;
+    const date = new Date(timestamp);
+
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const hours = date.getUTCHours();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = String(date.getUTCMinutes()).padStart(2, "0");
+
+    const formattedDate = `${
+      months[date.getUTCMonth()]
+    } ${date.getUTCDate()}, ${date.getUTCFullYear()} ${formattedHours}:${formattedMinutes} ${ampm} IST`;
+
+    return formattedDate;
+  };
+
   return (
     <>
       <h1>Infinite Scroll</h1>
       <div className="data-list">
         {data.map((data) => (
           <div className="data" key={data.node.nid}>
-            <div className="left">
-              <img
-                src={data.node.field_photo_image_section}
-                alt={data.node.title}
-              />
-            </div>
-            <div className="right">
-              <p>{data.node.title}</p>
-              <p>Last Update: {data.node.last_update}</p>
+            <div className="row">
+              <div className="left">
+                <img
+                  src={data.node.field_photo_image_section}
+                  alt={data.node.title}
+                />
+              </div>
+              <div className="right">
+                <p className="title">{data.node.title}</p>
+                <p className="time">{formattedDate(data.node.last_update)}</p>
+              </div>
             </div>
           </div>
         ))}
