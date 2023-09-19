a9dfb879c332210a148c36bb145e05488e7688ad
Akhil Mishra <akhilmishra184@gmail.com>
2023-09-20 01:31:33 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import InfiniteScrollView from './Component/InfiniteScrollView';
 
 function App() {
   return (
     <div className="App">
-     Hello
+     <InfiniteScrollView/>
     </div>
   );
 }
---
+++ src/Component/InfiniteScrollView.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const InfiniteScrollView = () => {
+  return <div>InfiniteScrollView</div>;
+};
+export default InfiniteScrollView;
