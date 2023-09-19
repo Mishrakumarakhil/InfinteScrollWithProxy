e5a8815cf16b069b53c3766a0b9e4aff55e1de14
Akhil Mishra <akhilmishra184@gmail.com>
2023-09-20 01:49:49 +0530

    added list view

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const  Card=(props)=> {
-  return (
-    <div>Card</div>
-  )
+const  Card=({data,loading})=> {
+
+     return (
+    <div>
+      <h1>Infinite Scroll</h1>
+      <div className="data-list">
+        {data.map((data) => (
+          <div className="data.node" key={data.node.nid}>
+            <img src={data.node.field_photo_image_section} alt={data.node.title} />
+            <p>{data.node.title}</p>
+            <p>Author: {data.node.author_name}</p>
+            <p>Last Update: {data.node.last_update}</p>
+          </div>
+        ))}
+      </div>
+      {loading && <p>Loading...</p>}
+  
+    </div>
+  );
 }
 
 export default  Card
--- src/Component/InfiniteScrollView.js
+++ src/Component/InfiniteScrollView.js
@@ -1,34 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Card from "./Card";
 
 const InfiniteScrollView = () => {
-    const [data, setData] = useState([]);
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
-    const API_URL = "app-api/v1/photo-gallery-feed-page/page";
-    const fetchData = async () => {
-        try {
-          setLoading(true);
-          const response = await axios.get(`${API_URL}/${page}`);
-          const newArticles = response.data;
-    
-    
-          setData([...data, ...newArticles.nodes]);
-          setPage(page + 1);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        } finally {
-          setLoading(false);
-        }
-      };
-    
-      useEffect(() => {
-        fetchData();
-      }, []);
-    
-   
+  const API_URL = "app-api/v1/photo-gallery-feed-page/page";
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${API_URL}/${page}`);
+      const newArticles = response.data;
 
-  return <div>InfiniteScrollView</div>;
+      setData([...data, ...newArticles.nodes]);
+      setPage(page + 1);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Card data={data} loading={loading} />
+    </div>
+  );
 };
 export default InfiniteScrollView;
